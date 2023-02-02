58a687eb4446170f8fe03ee519be44885f07cf4a
EugeneDashko <eugenedashko@yahoo.com>
2023-02-02 16:47:57 +0300

    JS video #1 time:31:00

--- src/js/app.js
+++ src/js/app.js
@@ -2,27 +2,35 @@ import * as flsFunction from "./modules/functions.js"
 
 flsFunction.isWebp();
 
-const nav =document.querySelector('#nav');
-const navBtn = document.querySelector('#nav__btn');
-const navBtnImg = document.querySelector('#nav__btn-img');
 
-// navBtn.onclick = () => {
-//     if (nav.classList.toggle('open')) { //Метод toggle объекта classList чередует заданный CSS класс элемента: добавляет класс, если его нет и удаляет, если есть. 
-//         navBtnImg.src ='../img/XX.svg'; // toggle возвращает если true
-//     } else {
-//         navBtnImg.src ='../img/X.svg'; // toggle возвращает если false
-//     }
-// }
-//
-// AOS.init({
-//     disable: 'phone', // не работает на мобильных устройствах
-//     once: true
-// });
+// document.querySelector('.order__wrap-title').addEventListener('click',() => {
+//       document.querySelector('.order').classList.toggle('order_open')
+//     });
 
-document.querySelector('.order__wrap-title').addEventListener('click',() => {
-    document.querySelector('.order').classList.toggle('order_open')
-  });
+//     document.querySelector('.order__close').addEventListener('click', () => {
+//       document.querySelector('.order').classList.toggle('order_open')
+//   });
 
-  document.querySelector('.order__close').addEventListener('click', () => {
-    document.querySelector('.order').classList.toggle('order_open')
-  });
+let modalProduct = document.querySelector('.modal_product');
+let catalogList = document.querySelector('.catalog__list');
+
+catalogList.addEventListener('click', (event) => {
+
+  const target = event.target;
+  console.log(target)
+
+  if (target.closest('.product_detail')) {
+    console.log('hi')
+    modalProduct.classList.add('modal_open')
+  }
+});
+
+
+modalProduct.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.closest('.modal__close') || target === modalProduct  ) {
+      modalProduct.classList.remove('modal_open')
+    }
+    // // modalProduct.classList.remove('modal_open')
+
+  })
